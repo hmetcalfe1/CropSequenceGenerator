2348d883c7e780ad75e991b229f83805e597373a
Ryan Sharp <ryan.sharp@rothamsted.ac.uk>
2021-06-03 16:57:13 +0100

    Added ability to read steady state and crop transition files. Added spring wheat to wheat rule. Added ability to turn off crop rules. Added nuts and soil loops.

--- scripts/resetP.m
+++ scripts/resetP.m
@@ -1,29 +1,36 @@
 function P = resetP(...
-    initialfreq,field,numcrops, potcounter,beetcounter,osrcounter,grass1counter,whcounter,maizecounter,...
-    grass2counter,potcropnum,beetcropnum,osrcropnum,grass1cropnum,whcropnum,maizecropnum,grass2cropnum,whmax,maizemax,grass2max...
+    initialfreq,field,numcrops,addCropRules...
+    ,potcounter,beetcounter,osrcounter,grass1counter,whcounter,maizecounter,grass2counter...
+    ,potcropnum,beetcropnum,osrcropnum,grass1cropnum,whcropnum,maizecropnum,grass2cropnum,swcropnum...
+    ,whmax,maizemax,grass2max...
 )
     P=initialfreq;
-    if potcounter(field,1)>0 %if we are not allowed to grow potatoes remove them from the probability matrix
-        P(potcropnum)=0;
+    
+    if addCropRules
+        if potcounter(field,1)>0 %if we are not allowed to grow potatoes remove them from the probability matrix
+            P(potcropnum)=0;
+        end
+        if beetcounter(field,1)>0 %if we are not allowed to grow beet remove them from the probability matrix
+            P(beetcropnum)=0;
+        end
+        if osrcounter(field,1)>0 %if we are not allowed to grow osr remove them from the probability matrix
+            P(osrcropnum)=0;
+        end
+        if grass1counter(field,1)>0 %if we are not allowed to grow grass1 remove them from the probability matrix
+            P(grass1cropnum)=0;
+        end
+        if whcounter(field,1)>=whmax %if we are not allowed to grow wheat remove them from the probability matrix
+            P(whcropnum)=0;
+            P(swcropnum)=0;
+        end
+        if maizecounter(field,1)>=maizemax %if we are not allowed to grow maize remove them from the probability matrix
+            P(maizecropnum)=0;
+        end
+        if grass2counter(field,1)>=grass2max %if we are not allowed to grow maize remove them from the probability matrix
+            P(grass2cropnum)=0;
+        end
     end
-    if beetcounter(field,1)>0 %if we are not allowed to grow beet remove them from the probability matrix
-        P(beetcropnum)=0;
-    end
-    if osrcounter(field,1)>0 %if we are not allowed to grow osr remove them from the probability matrix
-        P(osrcropnum)=0;
-    end
-    if grass1counter(field,1)>0 %if we are not allowed to grow grass1 remove them from the probability matrix
-        P(grass1cropnum)=0;
-    end
-    if whcounter(field,1)>=whmax %if we are not allowed to grow wheat remove them from the probability matrix
-        P(whcropnum)=0;
-    end
-    if maizecounter(field,1)>=maizemax %if we are not allowed to grow maize remove them from the probability matrix
-        P(maizecropnum)=0;
-    end
-    if grass2counter(field,1)>=grass2max %if we are not allowed to grow maize remove them from the probability matrix
-        P(grass2cropnum)=0;
-    end
+
     Ptotal=sum(P);
     for i=1:numcrops %Go through each crop
         P(i)=P(i)/Ptotal;%rescale the other propoertions
